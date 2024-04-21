ddbb54bdbcf9ed5bf4966ccefa8c4e9cffef0819
fridolph <249121486@qq.com>
2024-04-21 08:41:33 +0800

    chore(static): :lipstick: 相关静态资源和样式文件

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   // 兼容小程序，将 : 替换成 __
   separator: '__',
   theme: {
     // 兼容小程序，将默认配置里带 .和/ 清除
+
+    // 字体颜色
+    colors: {
+      3: '#333',
+      4: '#444',
+      5: '#555',
+      6: '#666',
+      7: '#777',
+      8: '#888',
+      9: '#999',
+      primary: '#3c9cff',
+      e: '#eee',
+      cell: '#e7f2f9',
+      gap: '#cbd5e1',
+    },
   },
   variants: {},
   plugins: [],
@@ -17,6 +32,6 @@ module.exports = {
     divideColor: false,
     divideOpacity: false,
     divideStyle: false,
-    divideWidth: false
-  }
-};
+    divideWidth: false,
+  },
+}
