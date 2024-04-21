7919ad9dc4488cf1d541b2d3c6bb33260ad895f3
fridolph <249121486@qq.com>
2024-04-21 12:13:27 +0800

    feat(resume-detail): :sparkles: 详情入口，基本信息模块

---
+++ src/pages/resume-detail/1baseinfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <uv-cell-group>
+    <uv-cell :cellStyle="{ backgroundColor: '#e7f2f9' }" :border="false">
+      <template v-slot:icon>
+        <Iconfont size="20" class="-translate-x-[2px]" name="mingpian" />
+      </template>
+      <template #title>
+        <text class="text-base">个人信息</text>
+      </template>
+    </uv-cell>
+    <uv-cell title="姓名" value="付寅生"></uv-cell>
+    <uv-cell title="工作年限" value="8年"></uv-cell>
+    <uv-cell title="学历" value="全日制本科"></uv-cell>
+  </uv-cell-group>
+
+  <uv-gap height="24rpx" bgColor="#eee" />
+
+  <uv-cell-group>
+    <uv-cell :cellStyle="{ backgroundColor: '#e7f2f9' }" :border="false">
+      <template v-slot:icon>
+        <Iconfont size="20" class="-translate-x-[2px]" name="jiaoyu" />
+      </template>
+      <template #title>
+        <text class="text-base text-3">教育经历</text>
+      </template>
+    </uv-cell>
+
+    <uv-cell title="毕业院校" value="四川大学锦江学院"></uv-cell>
+    <uv-cell title="专业" value="通信工程"></uv-cell>
+    <uv-cell title="就读时间" value="2012.9 —— 2016.7"></uv-cell>
+
+  </uv-cell-group>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/resume-detail/index.vue
@@ -0,0 +1,167 @@
+<template>
+  <uv-loading-page
+    :loading="loading"
+    loading-mode="spinner"
+    loading-text="Never give up!"
+    image="/static/logo.jpg"
+    icon-size="100rpx"
+  ></uv-loading-page>
+
+  <uv-skeletons :loading="loadingData" :skeleton="skeleton"></uv-skeletons>
+
+  <view v-if="!loading" class="resume-detail bg-slate-500 overflow-hidden">
+    <uv-notice-bar
+      class="h-[36.5rpx]"
+      text="欢迎访问我的简历项目。除了小程序您还可以在我的在线域名访问到，https://resume.fridolph.top 喜欢的话可以点个赞并收藏哦"
+      direction="row"
+      icon="integral"
+    ></uv-notice-bar>
+
+    <uv-vtabs
+      class="my-resume-tab"
+      :chains="true"
+      :height="tabHeight"
+      :list="list"
+      :barItemBadgeStyle="{ right: '6rpx', top: '42rpx' }"
+      barWidth="160rpx"
+      hdHeight="74rpx"
+      @change="clickTabItem"
+    >
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="0"
+        key="基本信息"
+        name="基本信息"
+        keyName="基本信息"
+      >
+        <Baseinfo />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="1"
+        key="联系方式"
+        name="联系方式"
+        keyName="联系方式"
+      >
+        <Contact />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="2"
+        key="专业技能"
+        name="专业技能"
+        keyName="专业技能"
+      >
+        <Skill />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="3"
+        key="工作经历"
+        name="工作经历"
+        keyName="工作经历"
+      >
+        <Experience />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="4"
+        key="项目经历"
+        name="项目经历"
+        keyName="项目经历"
+      >
+        <Program />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="5"
+        key="其他参与"
+        name="其他参与"
+        keyName="其他参与"
+      >
+        <Others />
+      </uv-vtabs-item>
+      <uv-vtabs-item
+        :barItemStyle="{ height: '50rpx' }"
+        index="6"
+        key="致谢"
+        name="致谢"
+        keyName="致谢"
+      >
+        <Thanks />
+      </uv-vtabs-item>
+    </uv-vtabs>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import Baseinfo from './1baseinfo.vue'
+import Contact from './2contact.vue'
+import Skill from './3skill.vue'
+import Experience from './4experience.vue'
+import Program from './5program/index.vue'
+import Others from './6others.vue'
+import Thanks from './7thanks.vue'
+import { onReady } from '@dcloudio/uni-app'
+
+/* ------- 页面初始化与加载 ------- */
+const loading = ref(true)
+// 模拟请求接口数据
+onReady(() => {
+  setTimeout(() => {
+    list.value = getList
+    loading.value = false
+  }, 666)
+})
+
+// tabbar相关
+const list = ref<any>([])
+const currentTab = ref<number>(0)
+const tabHeight = computed(() => {
+  // 这里因为要转rpx 所以在原有px基础上 * 2
+  return uni.getSystemInfoSync().windowHeight - uni.upx2px(74)
+})
+
+function clickTabItem(index: any) {
+  console.log('选项改变：', index)
+  currentTab.value = index
+}
+
+const getList = [
+  {
+    name: '基本信息',
+  },
+  {
+    name: '联系方式',
+  },
+  {
+    name: '专业技能',
+  },
+  {
+    name: '工作经历',
+    badge: {
+      isDot: true,
+    },
+  },
+  {
+    name: '项目经历',
+  },
+  {
+    name: '其他参与',
+  },
+  {
+    name: '致谢',
+  },
+]
+
+</script>
+
+<style lang="postcss">
+.resume-detail {
+  .uv-list-item {
+    margin-top: 12rpx;
+    font-size: 14px;
+  }
+}
+</style>
