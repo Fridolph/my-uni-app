e8b74c563f41f5f3b7122a23cec82843242ca50f
fridolph <249121486@qq.com>
2024-04-29 00:03:29 +0800

    feat(hobby-game): :sparkles: 游戏世界 > 推荐，类似vx朋友圈布局

--- src/pages/hobby-game/index.vue
+++ src/pages/hobby-game/index.vue
@@ -1,41 +1,58 @@
 <template>
-  <view class="relative w-full pb-[40%] overflow-hidden">
+  <view class="relative w-full pb-[40vw] overflow-hidden">
     <image
       class="absolute left-0 top-0 w-full h-full"
       src="https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/230902095822177.jpg?x-oss-process=image/crop,x_0,y_0,w_600,h_200"
     />
   </view>
-  <view class="p-[24rpx]">
+  <view
+    class="left-0 w-full h-[calc(100vh-40vw)] box-border overflow-x-hidden overflow-y-auto p-[24rpx]"
+  >
     <view class="mb-[24rpx]" v-for="item in 3" v-if="loadData">
       <uv-skeleton :loading="loadData" avatar avatarSize="40" rows="3"></uv-skeleton>
     </view>
-    <view v-if="!loadData" class="flex gap-[32rpx] mb-[48rpx]">
-      <uv-avatar src="https://via.placeholder.com/200x200.png/2878ff"></uv-avatar>
-      <view>
-        <view class="mb-[18rpx]">
-          uv-ui
-          破釜沉舟之兼容vue3+2、app、h5、小程序等多端基于uni-app和uView2.x的生态框架，支持单独导入，开箱即用，利剑出击。
+    <view v-if="!loadData">
+      <view v-for="item in list" :key="item.name" class="flex gap-[32rpx] mb-[48rpx]">
+        <uv-avatar :src="item.avatar"></uv-avatar>
+        <view>
+          <view class="mb-[12rpx] font-bold">
+            {{ item.name }}
+          </view>
+          <view class="mb-[24rpx] text-6">
+            {{ item.desc }}
+          </view>
+          <uv-album rowCount="2" :urls="item.images" :multipleSize="size" space="20rpx"></uv-album>
         </view>
-        <uv-album rowCount="2" :urls="urls" :multipleSize="size" space="20rpx"></uv-album>
       </view>
+
+      <!-- <view class="flex gap-[32rpx] mb-[48rpx]">
+        <uv-avatar src="https://via.placeholder.com/200x200.png/2878ff"></uv-avatar>
+        <view>
+          <view class="mb-[18rpx]">
+            uv-ui
+            破釜沉舟之兼容vue3+2、app、h5、小程序等多端基于uni-app和uView2.x的生态框架，支持单独导入，开箱即用，利剑出击。
+          </view>
+          <uv-album rowCount="2" :urls="urls" :multipleSize="size" space="20rpx"></uv-album>
+        </view>
+      </view> -->
+    </view>
+    <view class="wrap">
+      <uv-load-more loadmoreText="点击加载更多" loadingText="数据正在加载" :status="status" :line="true" @loadmore="loadMore" />
     </view>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { onLoad } from '@dcloudio/uni-app'
+import { onLoad, onReachBottom } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import { data, data2, data3 } from './mock'
+import type { IData } from './mock'
 
 const loadData = ref<boolean>(true)
-
-const urls = ref([
-  'https://via.placeholder.com/100x200.png/3c9cff/fff',
-  'https://via.placeholder.com/200x200.png/3c9cff/fff',
-  'https://via.placeholder.com/300x200.png/3c9cff/fff',
-  'https://via.placeholder.com/280x200.png/3c9cff/fff',
-])
-
+const status = ref<string>('loadmore')
 const size = ref<number>(80)
+const page = ref<number>(1)
+const list = ref<IData[]>([])
 
 onLoad(() => {
   const { screenWidth } = uni.getWindowInfo()
@@ -43,9 +60,38 @@ onLoad(() => {
   size.value = (screenWidth - 48 - 32 - 20) / 2
 
   setTimeout(() => {
+    list.value = data
     loadData.value = false
-  }, 1200)
+  }, 999)
 })
+
+function loadMore() {
+  status.value = 'loading'
+
+  setTimeout(() => {
+    if (page.value === 1) {
+      page.value = page.value + 1
+      list.value = [...list.value, ...data2]
+      status.value = 'loadmore'
+    } else if (page.value === 2) {
+      page.value = page.value + 1
+      list.value = [...list.value, ...data3]
+      status.value = 'loadmore'
+    } else {
+      status.value = 'nomore'
+    }
+  }, 1500)
+  // console.log(`loadMore -->`)
+}
+// onReachBottom(() => {
+//   if (page.value >= 2) return
+//   status.value = 'loading'
+//   page.value = ++page.value
+//   setTimeout(() => {
+//     if (page.value >= 3) status.value = 'nomore'
+//     else status.value = 'loading'
+//   }, 2000)
+// })
 </script>
 
 <style lang="scss">
--- src/pages/hobby-game/mock.ts
+++ src/pages/hobby-game/mock.ts
@@ -1,40 +1,61 @@
-import { ref } from 'vue'
+export interface IData {
+  name: string
+  avatar: string
+  desc: string
+  images: string[]
+}
 
-export const indexList = ref([
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-])
+export const data: IData[] = [
+  {
+    name: '异度之刃',
+    avatar:
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/yan.jpeg?x-oss-process=image/crop,x_180,y_0,w_260,h_260',
+    desc: '我最喜欢的游戏系列，没有之一。Switch独占，特别是第2部，中二少年的王道成长之路。',
+    images: [
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/blade.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/laopo.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/rex.png?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/noa.jpg?x-oss-process=image/resize,h_400,m_lfit',
+    ],
+  },
+  {
+    name: '命运石之门',
+    avatar:
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/zhushou.png?x-oss-process=image/resize,w_200,m_lfit',
+    desc: '独一档的优秀剧情，逻辑设定，各种伏笔和回收伏笔。优秀的Bgm和恰到好处的插入时机。多的不说了，一切都是命运石之门的选择！El Psy Congroo！',
+    images: [
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mingyun.jpg',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/dululu.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/dgzs.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/tina.jpg?x-oss-process=image/resize,h_400,m_lfit',
+    ],
+  },
+]
 
-export const itemArr = ref([
-  ['列表A1', '列表A2', '列表A3'],
-  ['列表B1', '列表B2', '列表B3'],
-  ['列表C1', '列表C2', '列表C3'],
-  ['列表D1', '列表D2', '列表D3'],
-  ['列表E1', '列表E2', '列表E3'],
-  ['列表F1', '列表F2', '列表F3'],
-  ['列表G1', '列表G2', '列表G3'],
-])
+export const data2: IData[] = [
+  {
+    name: 'Fate Stay Night',
+    avatar: 'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/shiro.jpg',
+    desc: 'ubw中士郎通过与红a的战斗明白了自己梦想的艰辛，但是依旧选择了走下去，这一条线个人认为有凛的陪伴，是最可能实现理想的士郎hf是三线中升华的一条线，前面俩条线玩完可以了解士郎对自己的了解是多么执着，而hf中为了樱放弃了自己的理想，可以想象士郎下了多大的决心，尤其在玩完前面俩条线之后看到hf的士郎说出要成为樱一个人正义的伙伴的时候那种感动深深地震撼了当时的我，至此彻底入了型月这个大坑。',
+    images: [
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/saber.jpg',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/ubw.jpg',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/lin.jpg',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/ying.png',
+    ],
+  },
+]
+
+export const data3: IData[] = [
+  {
+    name: '觅长生',
+    avatar: 'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mcs_a.png',
+    desc: '之前很沉迷的一款修仙游戏，Steam版。炼丹系统堪比复杂度堪比考研被网友甚为调侃，不过也确实如此，修仙界大佬太多，笔记做得跟花一样。玩起来确实很沉迷，几个门派build一遍几百小时就没了 … 可惜现在再也没有这样的游戏时光了。',
+    images: [
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mcs.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mcs2.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mcs4.jpg?x-oss-process=image/resize,h_400,m_lfit',
+      'https://fridolph.oss-rg-china-mainland.aliyuncs.com/my-uni-app/mcs3.jpg?x-oss-process=image/resize,h_400,m_lfit',
+    ],
+  },
+]
